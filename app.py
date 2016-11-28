d41285958948f07a79d441efcfd3f4bae37d750e
Jon Vlachogiannis <darksun4@gmail.com>
2016-11-27 22:13:41 -0800

    just a quick hack to play blind chess

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from flask import request, render_template
+from sunfish.sunfish import *
+
+app = Flask(__name__)
+
+pos = Position(initial, 0, (True,True), (True,True), 0, 0)
+searcher = Searcher()
+all_moves = ''
+steps = 1
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global pos
+    global searcher
+    global all_moves
+    global steps
+    
+    if pos.score <= -MATE_LOWER:
+        computer_move = "You lost"
+        
+    if request.method == 'POST':
+        move = request.form['move']
+        while move not in pos.gen_moves():
+            match = re.match('([a-h][1-8])'*2, move)
+            if match:
+                all_moves += str(steps)+'. ' + move + ' '
+                move = parse(match.group(1)), parse(match.group(2))
+            else:
+                # Inform the user when invalid input (e.g. "help") is entered
+                computer_move = "Please enter a valid move"
+        pos = pos.move(move)
+        if pos.score <= -MATE_LOWER:
+            computer_move = "You won"
+
+        move, score = searcher.search(pos, secs=2)
+
+        computer_move = str(render(119-move[0]) + render(119-move[1]))
+        pos = pos.move(move)
+        steps += 1
+        
+        if score == MATE_UPPER:
+            computer_move = "Checkmate"
+
+        all_moves += computer_move + ' '
+
+
+    return render_template('index.html', moves=all_moves)
+
+
+    
+@app.route('/reset', methods=['GET'])
+def reset():
+    global pos
+    global searcher
+    global all_moves
+    global steps
+    
+    pos = Position(initial, 0, (True,True), (True,True), 0, 0)
+    searcher = Searcher()
+    all_moves = ''
+    steps = 1
